b1b4c4d3a4616f6d1731c099a0d493ff49572480
arohpos <110832649+arohpos@users.noreply.github.com>
2023-05-27 17:38:17 +0900

    add: add the feature to distinguish log level

--- src/main/chap8/Exercise.java
+++ src/main/chap8/Exercise.java
@@ -1,19 +1,30 @@
 package main.chap8;
 
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Exercise {
 
 	public static void main(String[] args) {
-		//main.chap8.Exercise.javaを取得
-		Logger logger = LoggerFactory.getLogger("main.chap8.Exercise.java");
+		
+		String level;
+		System.out.println("ログの出力レベルを入力してください。");
+		System.out.println("[trace, debug, info, warn, error]");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		level = scanner.next();
+		Logger logger = LoggerFactory.getLogger(level);
 		
 		logger.trace("トレースレベルログ");
 		logger.debug("デバッグレベルログ");
 		logger.info("情報レベルログ");
 		logger.warn("警告レベルログ");
 		logger.error("エラーレベルログ");
+		
+		scanner.close();
 	}
 
 }
